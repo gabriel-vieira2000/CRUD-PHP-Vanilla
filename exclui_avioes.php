4e51d02f2c359c43a8f49daee7c83159b9b1a456
gabriel-vieira2000 <gvc20002016@gmail.com>
2021-04-29 10:15:15 -0300

    Criação do script para excluir um avião do banco de dados pelo seu ID

---
+++ exclui_avioes.php
@@ -0,0 +1,22 @@
+<?php
+    $idAviao = (isset($_GET['id'])) ? $_GET['id'] : 'null';
+
+    if ($idAviao == 'null'){
+        echo "<script>
+                alert('Você não selecionou um avião para ser excluído!');
+                window.location = 'avioes.php';
+            </script>";
+    }
+
+    require('conexaoBD.php');
+
+    $comandoSQL = "DELETE FROM AVIOES WHERE idAviao = '$idAviao'";
+
+    if(mysqli_query($con,$comandoSQL)){
+        echo "<script>alert('Registro excluído com sucesso!');window.location='avioes.php'</script>";
+    }else{
+        echo "<script>alert('Erro ao excluir o Avião Selecionado!');window.location='avioes.php'</script>";
+    }
+
+    mysqli_close($con);
+
